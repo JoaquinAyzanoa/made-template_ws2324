e5c95b92e80b14ec48ab0850785e30e0de01a4ba
JoaquinAyzanoa <joaquinayzanoa@gmail.com>
2023-12-11 23:26:34 +0100

    Create libraries requirements. Fixes JoaquinAyzanoa/made-template_ws2324#19

--- project/test.sh
+++ project/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+    
 # Install the requirements
-pip install -r ./project/requirements.txt
+python3 -m pip install -r ./project/requirements.txt
 
 # Run the pytest script
 python ./project/test.py
